02601a3e867962141c5c589023cdc31a6b4f21c5
arajesh001 <ashvathrajesh001@gmail.com>
2026-06-18 14:25:17 -0700

    initial files developed in spring quarter

---
+++ src/hardware_config.h
@@ -0,0 +1,18 @@
+# PIN NUMBERS ARE PLACEHOLDERS -> hardware team to edit 
+
+#pragma once
+
+// outputs
+#define OUTPUT_PIN -1
+
+// H-Bridge control
+#define POLARITY_PIN -1
+#define ENABLE_PIN -1
+
+// LEDC
+#define LEDC_CHANNEL 0
+#define LEDC_RESOLUTION 10
+#define LEDC_CARRIER_HZ -1
+
+// Optional
+#define FAULT_PIN -1
