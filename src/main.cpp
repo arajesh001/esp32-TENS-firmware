12aef26f56c79200c1caff164abcbb4195f32ad4
arajesh001 <ashvathrajesh001@gmail.com>
2026-06-19 11:23:57 -0700

    made main.cpp that runs the code in waveformGenerator class in an example case (1)

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include "waveform.h"
+#include "config.h"
+#include "safety.h"
+
+// swap mode to 2, or 3 to test other modes
+WaveformGenerator generator(1);
+
+void setup() {
+    Serial.begin(115200);
+
+    ledcSetup(LEDC_CHANNEL, LEDC_CARRIER_HZ, LEDC_RESOLUTION);
+    ledcAttachPin(OUTPUT_PIN, LEDC_CHANNEL);
+
+    pinMode(POLARITY_PIN, OUTPUT);
+    pinMode(ENABLE_PIN, OUTPUT);
+
+    digitalWrite(POLARITY_PIN, HIGH);
+    digitalWrite(ENABLE_PIN, LOW);
+    digitalWrite(ENABLE_PIN, HIGH);
+}
+
+void loop() {
+    generator.run();
+}
