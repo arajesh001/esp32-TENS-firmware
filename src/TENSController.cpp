cdf9f28cf1e8627eadbd3776025bcc6f6e0a558f
arajesh001 <ashvathrajesh001@gmail.com>
2026-06-19 16:11:16 -0700

    added the controller methods

--- src/TENSController.cpp
+++ src/TENSController.cpp
@@ -1,9 +1,32 @@
 #include "TENSController.h"
 
-TENSController::TENSController() {}
+TENSController::TENSController() {
+    _generator = nullptr;
+    _session_start_ms = 0;
+    _mode = 0;
+    _running = false;
+}
 
-void TENSController::startSession(uint8_t mode) {}
+void TENSController::stopSession() {
+    _running = false;
+    delete _generator;
+    _generator = nullptr;
+}
 
-void TENSController::stopSession() {}
+void TENSController::startSession(uint8_t mode) {
+    if (_running) {stopSession();}
 
-void TENSController::update() {}
+    _generator = new WaveformGenerator(mode);
+    _session_start_ms = millis();
+    _running = true;
+    _mode = mode;
+}
+
+void TENSController::update() {
+    if (MAX_SESSION_DURATION_MS < (millis() - _session_start_ms)) {
+        stopSession();
+    }
+    else if (_running && _generator != nullptr) {
+        _generator->run();
+    }
+}
